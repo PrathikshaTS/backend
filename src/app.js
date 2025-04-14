25408282cc69f1227c32011eee37468f7bec53f9
PrathikshaTS <prathikshats6105@gamil.com>
2025-04-14 11:39:57 +0530

    setup all done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"
 
 const app = express();
 app.use(
@@ -13,4 +13,9 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users",userRouter)
+
 export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import e from 'express';
+import {asyncHandler} from '../utils/asyncHandler.js';
+
+const registerUser=asyncHandler(async(req,res)=>{
+    res.status(200).json({
+        message:"ok"
+    })
+})
+export {registerUser}
