c42855cde62c8eec0dae29c5ff4e39cb01bed11f
PrathikshaTS <prathikshats6105@gamil.com>
2025-04-14 09:17:31 +0530

    usermodel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
     path: "./.env",
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT)
+})
+.catch((err) => {
+    console.log("Database connection error!!", err);
+    process.exit(1);
+})
